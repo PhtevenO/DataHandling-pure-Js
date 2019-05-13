dc1c82f12409b84ad047a31299930f10a247b9b7
Ed <edvinas.petrosius@gmail.com>
2019-05-13 23:12:41 +0300

    Progress

--- javacript/javascript.js
+++ javacript/javascript.js
@@ -2,34 +2,14 @@ var objData = data;
 
 // console.log(objData);
 
-
+var k = 0;
 function printValues(obj) {
-
-    var initKey = 'size';
-    var initName = obj['name'];
-    
     for (var key in obj) {
-        // console.log(key);
-        // console.log(obj);
-        var k = 0;
-        
         if (typeof obj[key] === "object") {
             printValues(obj[key]);
-            console.log(obj);
         } else {
-            if (k > 0){
-                console.log(initName = obj['name'] + ' - ' + obj['size']);
-                k++;
-            } else {
-                console.log(initName = obj['name'] + ' ' + obj['size']);
-                k++;
-            }
-            
-         
-            // console.log(obj[key]);
-           
+            console.log(obj[key]);
         }
-        
     }
 }
 
